b8433889925d427d24ad4c1ea3ad31e4897c522f
Ryan Foster-Hill <rfoster-hill@hotmail.com>
2023-07-26 17:19:24 +0100

    basic functionality finished

--- src/App.js
+++ src/App.js
@@ -1,182 +1,112 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
-import {add, subtract, multiply, divide, numericDependencies, create} from 'mathjs';
+import {add, subtract, multiply, divide, numericDependencies, create, evaluate} from 'mathjs';
 
 function App() {
 
-  const [buttonsDisplayCharactersArray, setButtonsDisplayCharatersArray] = useState([
+
+  const buttonsDisplayCharactersArray = [
     '7', '8', '9', 'C',
     '4', '5', '6', '*',
     '1', '2', '3', '/', 
     '+', '0', '-', '=',
-  ]);
+  ];
 
-  // let totalNumberAsStringDisplayRef = useRef();
+  //Displays whole typed string
+  const [currentCalculation, setCurrentCalculation] = useState('');
 
+  //Displays final calculated number
+  const [calculatedNumber, setCalculatedNumber] = useState(0);
 
+  //Displays erorr messages
+  const [erorrMessage, setErrorMessage] = useState("");
+  
+  //User is continuing calculation after initial calculation bool
+  const [calcIsContinued, setCalcIsContinued] = useState(false);
 
-  let totalNumberAsString = '';
-  let [totalNumberAsStringDisplay, setTotalNumberAsString] = useState('');
+  const clickHandler = (button) => {
+    if (button !== '=' && button !== 'C') { //Checks if button is not an equals or C
+      
+      if (button === '+' || button === '-' || button === '*' || button === '/') { //Checks if button is an operator
+        if (currentCalculation.length === 0) {
+          setErrorMessage("Error: First character cannot be an operator");
+          return;
+        }
+        //Checks if last char in current calculation is an operator
+        if (currentCalculation[currentCalculation.length - 1] === '+' || currentCalculation[currentCalculation.length - 1] === '-' || currentCalculation[currentCalculation.length - 1] === '*' || currentCalculation[currentCalculation.length - 1] === '/') {
+          //Log error message, can't have multiple operators next to each other
+          setErrorMessage(`Error: Cannot have multiple operators in succession`);
 
+        } else { //Add operator to sum string
+          setCurrentCalculation(currentCalculation + button);
 
+        }
 
-  // let finalCalculatedNumberDisplay = useRef();
-  let finalCalculatedNumber = 0;
-  // let [finalCalculatedNumberDisplay, setFinalCalculatedNumber] = useState(0);
-  let numbersToCalculateStringArray = [];
-  let calculationOperatorsStringArray = [];
-  let operatorArrayIndex = 0;
-  let numberArrayIndex = 0; //Need to store current place in numbers array instead of using i
+      } else { //Button must be a number at this point
 
-  //Resets calculator to initial state
-  const resetCalculator = () => {
-    totalNumberAsString = '';
-    numbersToCalculateStringArray = [];
-    calculationOperatorsStringArray = [];
-    finalCalculatedNumber = 0;
-    operatorArrayIndex = 0;
-    numberArrayIndex = 0;
-  };
+        if (calculatedNumber) { //Checks if a number has been calculated already using '='
+          //Checks if the last character in the sum string is an operator, if so then add number to sum string
+          if (currentCalculation[currentCalculation.length - 1] === '+' || currentCalculation[currentCalculation.length - 1] === '-' || currentCalculation[currentCalculation.length - 1] === '*' || currentCalculation[currentCalculation.length - 1] === '/' || calcIsContinued === true) {
+            setCurrentCalculation(currentCalculation + button); //Add number to sum string
+            setCalculatedNumber('');
+            setCalcIsContinued(true); //User is continuing the calculation so set to true
 
-  //Does maths
-  const doMath = () => {
-    // console.log(numbersToCalculateStringArray[0]);
-    
-    for (numberArrayIndex; numberArrayIndex < numbersToCalculateStringArray.length; numberArrayIndex++) {
+          } else { //If a calculation has already been done and a number is then entered, reset the calculator to initial state
+              setCurrentCalculation(button);
+              setCalculatedNumber('');
+          } 
 
-      //Performs initial calculation if no calculation has been done yet by checking if calculated number is 0
-      if (finalCalculatedNumber === 0) {
+          } else { //If a number has not already been calculated using '=', adds button to sum string
+            setCurrentCalculation(currentCalculation + button);
+            
+
+            // setCalculatedNumber(evaluate(currentCalculation + button));
+            
+            //When this runs, currentCalculation doesn't contain the button that was just added
+            // setCalculatedNumber(evaluate(currentCalculation));
+          }
         
-
-        if (calculationOperatorsStringArray[operatorArrayIndex] === '+') {
-          finalCalculatedNumber += Number(numbersToCalculateStringArray[numberArrayIndex]) + Number(numbersToCalculateStringArray[numberArrayIndex + 1]);
-
-        } else if (calculationOperatorsStringArray[operatorArrayIndex] === '-') {
-          finalCalculatedNumber += Number(numbersToCalculateStringArray[numberArrayIndex]) - Number(numbersToCalculateStringArray[numberArrayIndex + 1]);
-
-        }  else if (calculationOperatorsStringArray[operatorArrayIndex] === '*') {
-          finalCalculatedNumber += Number(numbersToCalculateStringArray[numberArrayIndex]) * Number(numbersToCalculateStringArray[numberArrayIndex + 1]);
-          
-        }  else if (calculationOperatorsStringArray[operatorArrayIndex] === '/') {
-          finalCalculatedNumber += Number(numbersToCalculateStringArray[numberArrayIndex]) / Number(numbersToCalculateStringArray[numberArrayIndex + 1]);
-          
-        } else {
-          console.log("Error in doMath function (intitial calculation), operator not recognized");
-          console.log(`This operator is not recognized: ${calculationOperatorsStringArray[operatorArrayIndex]}`);
-        }
-
-        numberArrayIndex++; //Increment numberArrayIndex in the initial calculation so its at the right index for the follow up calculations
-        operatorArrayIndex++;
-
-      } else if (finalCalculatedNumber !== 0) {
-        console.log("FOLLOW UP CALCULATION IS RUNNING");
-        //These calculations only run if the initial calculation has been done
-        if (calculationOperatorsStringArray[operatorArrayIndex] === '+') {
-          finalCalculatedNumber += Number(numbersToCalculateStringArray[numberArrayIndex]);
-
-        } else if (calculationOperatorsStringArray[operatorArrayIndex] === '-') {
-          finalCalculatedNumber -= Number(numbersToCalculateStringArray[numberArrayIndex]);
-
-        }  else if (calculationOperatorsStringArray[operatorArrayIndex] === '*') {
-          finalCalculatedNumber *= Number(numbersToCalculateStringArray[numberArrayIndex]);
-          
-        }  else if (calculationOperatorsStringArray[operatorArrayIndex] === '/') {
-          finalCalculatedNumber /= Number(numbersToCalculateStringArray[numberArrayIndex]);
-          
-        } else {
-          console.log("Error in doMath function(followup calculation), operator not recognized");
-          console.log(`This operator is not recognized: ${calculationOperatorsStringArray[operatorArrayIndex]}`);
-          console.log(`Operator array index: ${operatorArrayIndex}`);
-        }
-
-        operatorArrayIndex++;
-        console.log(`Operator array index AFTER INCREASE: ${operatorArrayIndex}`);
+        //Reset error message
+        setErrorMessage('');
       }
+
+    } else if (button === 'C') { //Checks if button is C, if so, resets calculator to initial state
+      setCurrentCalculation('');
+      setCalculatedNumber('');
+      setErrorMessage('');
+      setCalcIsContinued(false);
+
+    } else if (button === '=') { //Checks if button is =
+
+      //Checks if last character in string sum is an operator, if so then the sum must not be complete and so logs an erorr message
+      if (currentCalculation[currentCalculation.length - 1] === '+' || currentCalculation[currentCalculation.length - 1] === '-' || currentCalculation[currentCalculation.length - 1] === '*' || currentCalculation[currentCalculation.length - 1] === '/') {
+        setErrorMessage("Error: This is not a calculable sum");
+      } else //Performs the maths sum and resets error message
+      {
+              setCalculatedNumber(evaluate(currentCalculation));
+              setErrorMessage('');
+              setCalcIsContinued(false);
+      }
+
+    } else { //If none of the other if statements run then an error must have occurred
+      setErrorMessage("An erorr occurred in click handler");
     }
-
-    // console.log(Number(numbersToCalculateStringArray[0]));
-    console.log(`finalCalculatedNumber = ${finalCalculatedNumber}`);
-
-
-    //The output of the calculation being 0 acts as a calculator reset
-    if (finalCalculatedNumber === 0) {
-      resetCalculator(); //Reset calculator if number is 0 after all calculations
-      numbersToCalculateStringArray[0] = '0'; //Sets first number to calculate to 0 so it can used immediately
-    }
-
-    // setFinalCalculatedNumber(finalCalculatedNumber);
-    return finalCalculatedNumber;
   };
-
-  //Button click handler
-  const handleClick = (button) => {
-    if (button === 'C') {
-      //Clear Display
-      resetCalculator();
-
-    } else if (button === '+' || button === '-' || button === '*' || button === '/') {
-      //Store operator for use in calculation
-      calculationOperatorsStringArray.push(button); //Adds clicked operator to operator storage array
-      // calculationOperatorsStringArray += button;
-      
-      //Checks if number is empty, so as not to push an empty string to the number array
-      if (totalNumberAsString !== '') {
-        numbersToCalculateStringArray.push(totalNumberAsString); //Add new number to number storage array
-      }
-      totalNumberAsString = ''; //Resets current number string to empty
-
-    } else if (button === '=') {
-      //Do Equation
-      numbersToCalculateStringArray.push(totalNumberAsString); //Add final number to number storage array
-      totalNumberAsString = ''; //Resets current number string to empty
-      doMath();
-
-    } else {
-      //If button clicked is not an operator then it must be a number
-      //Attach number to total number string
-      
-      totalNumberAsString += button;
-      setTotalNumberAsString(totalNumberAsStringDisplay + button);
-      // let newString = totalNumberAsString += button
-      // setTotalNumberAsString(newString);
-    }
-
-    console.log(`Operator array: ${calculationOperatorsStringArray}`);
-    console.log(`Total number: ${totalNumberAsString}`);
-    console.log(`Numbers string array: ${numbersToCalculateStringArray}`);
-
-    // updateDisplay();
-
-    
-  }
-
-  const updateDisplay = () => {
-    // setTotalNumberAsString(totalNumberAsString);
-    // setFinalCalculatedNumber(finalCalculatedNumber);
-    setTotalNumberAsString(totalNumberAsString);
-    console.log(`UPDATE DISPLAY TOTAL NUMBER: ${totalNumberAsString}`);
-  }
 
   return (
     <div className="App">
-      <div>
-        <div id="currentNumberDisplay">
-          <p>Current Number:{totalNumberAsStringDisplay}</p>
-        </div>
-        <div id="operatorDisplay">
-
-        </div>
-
-        <div>
-        <p>Total Number: {}</p>
-        </div>
+      <div id="calculatorDisplay">
+        <p className='displayBox'>{erorrMessage}</p>
+        <p className='displayBox'>{currentCalculation}</p>
+        <p className='displayBox'>{calculatedNumber}</p>
       </div>
+
       <div id="buttonContainer">
         {buttonsDisplayCharactersArray.map((button, index) => {
-        return <button onClick={() => {handleClick(button)}} className='button' key={index}>{button}</button>
-      })}
+          return <button onClick={() => {clickHandler(button)}} className='button' key={index}>{button}</button>
+        })}
       </div>
-
+      
     </div>
   );
 }
